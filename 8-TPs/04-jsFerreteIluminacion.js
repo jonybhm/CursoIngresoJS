97a4c14ce98d56d21b26aef624dfe0934cc0fba1
unknown <jony.bhm@gmail.com>
2022-07-13 16:48:03 -0300

    TP4 y parcial de IF

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,14 +1,115 @@
 /*4.	Para el departamento de iluminación:
-Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
-A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
-B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
-C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
-D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+Tomando en cuenta que todas las lámparas están en oferta al mismo 
+precio de $35 pesos final.
+A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento 
+del 50%. 
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se 
+hace un descuento del 40 % y si es de otra marca el descuento es 
+del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" 
+o “FelipeLamparas” se hace un descuento del 25 % y si es de 
+otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  
+el descuento es del 15%, si es  “FelipeLamparas” se hace un 
+descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se 
+debe sumar un 10% de ingresos brutos en informar del impuesto 
+con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
-
- */
+*/
 function CalcularPrecio () 
 {
- 	
+    //cantidad txtIdCantidad
+    //tienda elegida Marca
+    //decuento txtIdprecioDescuento
+
+    let precioLampara;
+    let cantidad;
+    let marca;
+    let descuento;
+    let precioFinal;
+    let mensaje;
+    let impuesto;
+
+    precioLampara=35;
+    cantidad=document.getElementById("txtIdCantidad").value;
+    marca=document.getElementById("Marca").value;
+    
+    cantidad=parseFloat(cantidad);
+    descuento=parseFloat(descuento);
+    impuesto=parseFloat(impuesto);
+    
+    if(cantidad>5)
+    {
+        descuento=0.5;
+        precioFinal=(precioLampara*cantidad)-(precioLampara*cantidad*descuento);
+        document.getElementById("txtIdprecioDescuento").value=precioFinal;
+    }else
+    {
+        if(cantidad==5 && marca=="ArgentinaLuz")
+        {
+        descuento=0.4;
+        precioFinal=(precioLampara*cantidad)-(precioLampara*cantidad*descuento);
+        document.getElementById("txtIdprecioDescuento").value=precioFinal;
+        }else
+        {
+            if(cantidad==5 && marca!="ArgentinaLuz")
+            {
+                descuento=0.3;
+                precioFinal=(precioLampara*cantidad)-(precioLampara*cantidad*descuento);
+                document.getElementById("txtIdprecioDescuento").value="$"+precioFinal;
+            }else
+            {
+                if(cantidad==4 && marca=="ArgentinaLuz")
+                {
+                    descuento=0.25;
+                    precioFinal=(precioLampara*cantidad)-(precioLampara*cantidad*descuento);
+                    document.getElementById("txtIdprecioDescuento").value="$"+precioFinal;
+                }else
+                {
+                    if(cantidad==4 && marca!="ArgentinaLuz")
+                    {
+                        descuento=0.2;
+                        precioFinal=(precioLampara*cantidad)-(precioLampara*cantidad*descuento);
+                        document.getElementById("txtIdprecioDescuento").value="$"+precioFinal;
+                    }else
+                    {
+                        if(cantidad==3 && marca=="ArgentinaLuz")
+                        {
+                        descuento=0.15;
+                        precioFinal=(precioLampara*cantidad)-(precioLampara*cantidad*descuento);
+                        document.getElementById("txtIdprecioDescuento").value="$"+precioFinal;
+                        }else
+                        {
+                            if(cantidad==3 && marca=="FelipeLamparas")
+                            {
+                            descuento=0.1;
+                            precioFinal=(precioLampara*cantidad)-(precioLampara*cantidad*descuento);
+                            document.getElementById("txtIdprecioDescuento").value="$"+precioFinal;
+                            }else
+                            {
+                                if(cantidad==3 && marca!="FelipeLamparas" && marca!="ArgentinaLuz")
+                                {
+                                descuento=0.05;
+                                precioFinal=(precioLampara*cantidad)-(precioLampara*cantidad*descuento);
+                                document.getElementById("txtIdprecioDescuento").value="$"+precioFinal;
+                                }else
+                                {
+                                    precioFinal=precioLampara*cantidad;
+                                    document.getElementById("txtIdprecioDescuento").value="$"+precioFinal;
+                                }
+                            }
+                        }
+                    }
+                }                   
+            }                   
+        }
+    }
+    if(precioFinal>=120)
+    {
+        impuesto=precioFinal*0.1;
+        precioFinal=precioFinal+impuesto;
+        mensaje="Usted pago $"+ impuesto +" de IIBB.";
+        alert(mensaje);
+    }
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,51 @@
+/*
+Bienvenidos (IF).
+Pedir dos números y mostrar el resultado:  
+Si son iguales los muestro concatenados.
+Si el primero es mayor, los resto,
+de lo contrario los sumo.
+Si la suma es mayor a 10 ,además de mostrar el resultado,
+muestro el mensaje 
+"la suma es xxx y supero el 10".
+*/
 function mostrar()
 {
+	let numeroUno;
+	let numeroDos;
+	let mensaje;
+	let resultado;
 
+	numeroUno=prompt("ingrese el primer numero");
+	numeroDos=prompt("ingrese el segundo numero");
+
+	if(numeroUno==numeroDos)
+	{
+		mensaje=numeroUno + numeroDos;
+		alert(mensaje);
+	}else
+	{
+		if(numeroUno>numeroDos)
+		{
+			numeroUno=parseFloat(numeroUno);
+			numeroDos=parseFloat(numeroDos);
+			
+			resultado=numeroUno-numeroDos;
+			
+			alert(resultado);
+		}else
+		{
+			numeroUno=parseFloat(numeroUno);
+			numeroDos=parseFloat(numeroDos);
+			
+			resultado=numeroUno+numeroDos;
+			
+			alert(resultado);
+
+			if(resultado>10)
+			{
+				alert("la suma es "+ resultado +" y supero el 10")
+			}
+		}
+	}
+	
 }
