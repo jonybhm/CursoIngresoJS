7dfdeffb9eccdd9fbf2abfa9f010ec924d254efe
unknown <jony.bhm@gmail.com>
2022-07-27 21:43:23 -0300

    ejercicios 1 2 y 3 parcial 2020

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,132 @@
+/*
+Realizar el algoritmo que permita ingresar los datos de una compra de ingredientes para preparar comida al por mayor , 
+hasta que el cliente quiera:
+peso (entre 10 y 1000)en kilo,
+precio por kilo (más de cero ),
+tipo validad("v";"a";"m")(vegetal,animal o mezcla )
+Si compro más de 100 kilos en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 300 kilos en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo de alimento más caro.
+f) El promedio de precio por kilo en total.*/
+
+
 function mostrar()
 {
-  alert("dos");
+
+  let pesoComida;
+  let importePorKilo;
+  let tipoComida;
+  let descuento=0;
+  let mensaje;
+
+  let acumuladorPesoVegetal=0;
+  let acumuladorImporteVegetal=0;
+  let acumuladorPesoAnimal=0;
+  let acumuladorImporteAnimal=0;
+  let acumuladorPesoMezcla=0;
+  let acumuladorImporteMezcla=0;
+
+  let acumuladorPesoTotal=0;
+  let importeTotal=0;
+
+  let comidaMasCara;
+
+  let respuesta=true;
+
+  while(respuesta==true)
+  {
+    do
+    {
+      pesoComida=prompt("Ingrese el peso a comprar en Kg");
+      pesoComida=parseInt(pesoComida);
+    }while(isNaN(pesoComida) || pesoComida<10 || pesoComida>1000);
+
+    do
+    {
+      importePorKilo=prompt("Ingrese el importe por kilo");
+      importePorKilo=parseInt(importePorKilo);
+    }while(isNaN(importePorKilo) || importePorKilo<0);
+
+    do
+    {
+      tipoComida=prompt("Ingrese el tipo de tipoComida").toLowerCase();
+
+    }while(!isNaN(tipoComida) || (tipoComida!="v" && tipoComida!="a" && tipoComida!="m"))
+
+    
+
+    switch(tipoComida)
+    {
+      case "v":
+        acumuladorPesoVegetal=pesoComida+acumuladorPesoVegetal;
+
+        acumuladorImporteVegetal=(importePorKilo*pesoComida)+acumuladorImporteVegetal;
+      break;
+
+      case "a":
+        acumuladorPesoAnimal=pesoComida+acumuladorPesoAnimal;
+
+        acumuladorImporteAnimal=(importePorKilo*pesoComida)+acumuladorImporteAnimal;
+      break;
+
+      case "m":
+        acumuladorPesoMezcla=pesoComida+acumuladorPesoMezcla;
+
+        acumuladorImporteMezcla=(importePorKilo*pesoComida)+acumuladorImporteMezcla;
+      break;
+    }
+
+    respuesta=confirm("desea continuar?");
+
+
+  }//fin while
+
+  if(acumuladorImporteAnimal > acumuladorImporteMezcla && acumuladorImporteAnimal > acumuladorImporteVegetal) 
+  { 
+    comidaMasCara = "Animal";
+  }
+  else
+  {
+    if(acumuladorImporteMezcla > acumuladorImporteAnimal && acumuladorImporteMezcla > acumuladorImporteVegetal) 
+    {
+      comidaMasCara = "Mezcla";
+    
+    }
+    else 
+    {
+      comidaMasCara = "Vegetal";
+      
+    }
+  }
+
+  acumuladorPesoTotal=acumuladorPesoMezcla+acumuladorPesoAnimal+acumuladorPesoVegetal;
+  importeTotal=acumuladorImporteMezcla+acumuladorImporteVegetal+acumuladorImporteAnimal;
+
+  if(acumuladorPesoTotal>100 && acumuladorPesoTotal<300)
+  {
+    descuento=0.85;
+  }else
+  {
+    if(acumuladorPesoTotal>300)
+    {
+      descuento=0.75;
+    }
+  }
+
+
+
+  mensaje="a) El importe total a pagar , bruto sin descuento es: "+ importeTotal;
+
+  if(descuento!=0)
+  {
+    mensaje+="\nb) el importe total a pagar con descuento "+ importeTotal*descuento;
+  }
+
+  mensaje+="\nd) El tipo de alimento más caro. "+comidaMasCara;
+  mensaje+="\nf) El promedio de precio por kilo en total. " + importeTotal/acumuladorPesoTotal;
+
+  alert(mensaje);
+
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,146 @@
+/*
+En el ingreso a un viaje en crucero nos solicitan 
+nombre , 
+edad(mayores de 18), 
+sexo("f" o "m") y 
+estado civil("soltero", "casado" o "viudo");
+a) El nombre del hombre casado más joven.
+b) El sexo y nombre del pasajero/a más viejo.
+c) La cantidad de mujeres que hay casadas o viudas.
+d) El promedio de edad entre las mujeres.
+e) El promedio de edad entre los hombres solteros. 
+*/
+
+
 function mostrar()
 {
-	alert("tres");
+	let nombre;
+	let edad;
+	let sexo;
+	let estadoCivil;
+
+	let flagPrimerIngreso=true;
+	
+	let nombreHombreCasadoMasJoven;
+	let edadHombreCasadoMasJoven;
+
+	let sexoPasajeroMasViejo;
+	let nombrePasajeroMasViejo;
+	let edadPasajeroMasViejo;
+
+	let acumuladorEdadMujeres=0;
+	let contadorMujeresCasadasViudas=0;
+	let contadorMujeresTotal=0;
+	let promedioEdadMujeres;
+
+	let acumuladorEdadHombresSolteros=0;
+	let contadorHombresSolteros=0;
+	let promedioEdadHombres;
+
+	let respuesta=true;
+
+	let mensaje;
+
+	while(respuesta==true)
+	{
+		do
+	    {
+	        nombre=prompt("Ingrese nombre");
+	        	    
+	    }while(!isNaN(nombre));
+
+	    do
+	    {
+	        edad=prompt("Ingrese edad, mayores de 18");
+
+	        edad=parseInt(edad);
+
+	    }while(isNaN(edad) || edad<18);
+
+		do
+	    {
+	        sexo=prompt("Ingrese sexo, “M” para masculino y “F” para femenino");
+	        sexo=sexo.toUpperCase();
+	    
+	    }while(!isNaN(sexo) || (sexo!="M" && sexo!="F"));
+
+	    do
+	    {
+	               
+	        estadoCivil=prompt("Ingrese sus estado Civil, soltero, casado o viudo").toLowerCase();
+	                
+	    }while(!isNaN(estadoCivil) || (estadoCivil!="soltero" && estadoCivil!="casado" && estadoCivil!="viudo"));
+
+	    if(flagPrimerIngreso==true)
+		{
+			nombreHombreCasadoMasJoven = nombre;
+			edadHombreCasadoMasJoven = edad;
+
+			sexoPasajeroMasViejo = sexo; 
+			nombrePasajeroMasViejo = nombre;
+			edadPasajeroMasViejo = edad;
+
+			flagPrimerIngreso = false;
+		}else
+		{
+			if(sexo=="M" && estadoCivil=="casado" && edad<edadHombreCasadoMasJoven)
+			{
+				edadHombreCasadoMasJoven=edad;
+				nombreHombreCasadoMasJoven = nombre;
+			}else
+			{
+				if(edad>edadPasajeroMasViejo)
+				{
+					edadPasajeroMasViejo=edad;
+					sexoPasajeroMasViejo = sexo; 
+					nombrePasajeroMasViejo = nombre;
+				}
+			}
+		}
+
+		switch(sexo)
+		{
+			case "F":
+				switch(estadoCivil)
+				{
+					case "casado":
+					case "viudo":
+						acumuladorEdadMujeres=acumuladorEdadMujeres+edad;
+						contadorMujeresCasadasViudas++;
+						contadorMujeresTotal++;
+					break;
+
+					default:
+						acumuladorEdadMujeres=acumuladorEdadMujeres+edad;
+						contadorMujeresTotal++;
+					break;
+				}
+			break;
+
+			case "M":
+				switch(estadoCivil)
+				{
+					case "soltero":
+						acumuladorEdadHombresSolteros=acumuladorEdadHombresSolteros+edad;
+						contadorHombresSolteros++;
+					break;
+				}
+			break;
+		}
+
+
+		respuesta=confirm("desea continuar?");
+	}
+
+	promedioEdadHombres=(acumuladorEdadHombresSolteros/contadorHombresSolteros).toFixed(2);
+	promedioEdadMujeres=(acumuladorEdadMujeres/contadorMujeresTotal).toFixed(2);
+
+	mensaje="a) El nombre del hombre casado más joven. "+nombreHombreCasadoMasJoven;
+	mensaje+="\nb) El sexo y nombre del pasajero/a más viejo."+sexoPasajeroMasViejo + nombrePasajeroMasViejo;
+	mensaje+="\nc) La cantidad de mujeres que hay casadas o viudas."+contadorMujeresCasadasViudas;
+	mensaje+="\nd) El promedio de edad entre las mujeres. "+promedioEdadMujeres;
+	mensaje+="\ne) El promedio de edad entre los hombres solteros. "+promedioEdadHombres;
+
+	alert(mensaje);
+
 }
